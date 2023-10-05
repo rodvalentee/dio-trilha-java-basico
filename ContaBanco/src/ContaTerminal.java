f1b397c81dabdd03b123d1701bf890f872376b56
rodvalentee <rodrigovalente.ti@gmail.com>
2023-10-04 23:34:02 -0300

    Exercicio Conta Banco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        int numero;
+        String agencia;
+        String nomeCliente;
+        float saldo;
+
+        System.out.println("Digite o numero da conta: ");
+        numero = sc.nextInt();
+        System.out.println("Digite o numero da agencia: ");
+        agencia = sc.next();
+        System.out.println("Digite o nome do cliente: ");
+        nomeCliente = sc.next();
+        System.out.println("Digite o saldo: ");
+        saldo = sc.nextFloat();
+
+        System.out.println("Olá " +  nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+    }
 }
