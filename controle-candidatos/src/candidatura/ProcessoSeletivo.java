b91d43f771a5fbcb8713720f0c8dc48b9e13cc4f
rodvalentee <rodrigovalente.ti@gmail.com>
2023-10-06 01:16:28 -0300

    Exercicio finalizado e pastas ajustadas

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,4 +1,44 @@
 package candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
+
+    public static void main(String[] args) {
+        analisarCandidato(1900);
+        analisarCandidato(2000);
+        analisarCandidato(2200);
+    }
+
+    static void selecaoCandidatos(){
+        // Array com a lista de candidatos
+        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
+
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+        while(candidatosSelecionados<5){
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+        }
+    }
+    static void analisarCandidato(double salarioPretendido){
+        double salarioBase = 2000.0;
+
+        if(salarioBase>salarioPretendido){
+            System.out.println("LIGAR PARA O CANDIDATO");
+        }
+        else if(salarioBase==salarioPretendido){
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
+        }
+        else{
+            System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
+        }
+
+    }
+
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
 }
